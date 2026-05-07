d724922f800846f31067d7c60b71405152c53e30
steven <stevenrios.0413@gmail.com>
2026-05-06 19:17:34 -0500

    feat: implement positive power endpoint

---
+++ Domain/Services/MathService.cs
@@ -0,0 +1,17 @@
+﻿namespace FundamentosPOO_ITM.Domain.Services
+
+{
+    public class MathService
+    {
+        public string PositivePower(int num)
+        {
+            if (num > 0)
+                return (num*num).ToString();
+
+            if (num == 0)
+                return "0";
+
+            return "Número negativo";
+        }
+    }
+}
